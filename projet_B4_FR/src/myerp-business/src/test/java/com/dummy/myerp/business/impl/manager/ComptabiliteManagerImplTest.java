de729bc672c7e7799b69fec1a5e3633a8aea0c33
Habib <thiernohabibbah@gmail.com>
2019-11-07 13:00:43 +0100

    T4

--- projet_B4_FR/src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplTest.java
+++ projet_B4_FR/src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplTest.java
@@ -624,5 +624,18 @@ public class ComptabiliteManagerImplTest {
 		assertEquals(true, res);
 	}
 
-	
+	@Test
+	public void testChekEC() throws FunctionalException {
+		EcritureComptable vEcritureComptable;
+		vEcritureComptable = new EcritureComptable();
+		vEcritureComptable.setJournal(new JournalComptable("AC", "Achat"));
+		vEcritureComptable.setDate(new Date());
+		vEcritureComptable.setLibelle("Libelle");
+		vEcritureComptable.getListLigneEcriture()
+				.add(new LigneEcritureComptable(new CompteComptable(1), null, new BigDecimal(123), null));
+		vEcritureComptable.getListLigneEcriture()
+				.add(new LigneEcritureComptable(new CompteComptable(2), null, null, new BigDecimal(123)));
+			manager.checkEcritureComptableUnit(vEcritureComptable);
+		
+	}
 }
